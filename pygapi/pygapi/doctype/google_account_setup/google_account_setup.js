f1bf03a9fc1dda484ead954662df021243f708fe
VamC <vwithv1602@gmail.com>
2017-12-29 19:29:10 +0530

    Updating sales@uyn.com and dynamic client id and secret fetching

---
+++ pygapi/pygapi/doctype/google_account_setup/google_account_setup.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, vavcoders and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Google Account Setup', {
+	refresh: function(frm) {
+
+	}
+});
